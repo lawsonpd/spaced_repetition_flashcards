f7379bce9cbde7d8c6270c3d612f80003759cb5f
Peter Lawson <lawsonpd@pm.me>
2022-06-19 20:33:30 -0500

    Change box representation to dict of box names. Change card representation to tuple of prompt-answer pair. Modify functions to handle data structure changes.

--- flash_cards.py
+++ flash_cards.py
@@ -3,48 +3,55 @@ import random
 from flask import Flask
 
 
-# Cards are represented as dictionary items. The key
-# is the "prompt" and the value is the "answer".
+# Cards are represented as tuples: (prompt, answer).
+# All elements are assumed to be strings.
 # E.g.,
-# {'2+2 = ___': 4},
-# {'What is "m" in "y = mx + b"?': 'slope'}
+# ('2+2 = ___', '4'),
+# ('What is "m" in "y = mx + b"?', 'slope')
 
-# Order of `boxes` matters. boxes[0] holds cards the learner
-# has had the most success with, boxes[1] holds cards the
-# learner has had some success with, and boxes[2] holds cards
-# the learner has had the least success with.
-boxes = [[], [], []] # Order: most success, some success, least success
+boxes = dict.fromkeys(['low', 'mid', 'high'], [])
 
-def spaced_rep_choice(n, cum_weights=(1,3,8)):
-    "Returns int in range n using weighted sampling"
-    return random.choices(range(n), cum_weights=cum_weights, k=1)
-
-def add_card(card, boxes):
+def spaced_rep_choice(boxes, cum_weights=(1,3,8)):
     '''
-    Card is a dict where key is 'prompt' and value is 'answer'.
-    Since a new card is assumed to be unlearned, it is by default
-    added to the "least success" box.
+    Returns one of els using weighted sampling.
+    
+    els is assumed to be the keys of boxes.
     '''
-    boxes[2].append(card)
-    return boxes
+    return random.choices(boxes.keys(), cum_weights=cum_weights, k=1)
 
-def select_box(boxes):
-    "Select a box from which to draw a card."
-    box_index = spaced_rep_choice(len(boxes))
-    return box_index
+def select_card(boxes):
+    '''
+    Choose a (weighted) random box and select the front card.
 
-def select_card(box_index, boxes):
-    return boxes[box_index][0]
+    Return dict with keys "from box" and "card": name (string) of
+    box from which the card was selected and card (prompt-answer tuple),
+    respectively.
 
-def card_prompt(card):
-    return
+    Note: Another method would be to select a random card, but, since
+    cards are (currently) appended to the end of a box after a trial,
+    this method guarantees that a card won't be selected twice in a row.
+    '''
+    box_key = spaced_rep_choice(boxes)
+    return {"from box": box_key, "card": boxes[box_key][0]}
+
+def get_card_prompt(card):
+    return card[0]
+
+def get_card_answer(card):
+    return card[1]
 
 def evaluate(card):
     pass
 
-def evaluate_test(result, card):
-    ""
-    pass
+def add_new_card(card, boxes):
+    '''
+    Card is a tuple of 'prompt' and 'answer'.
+
+    Since a new card is assumed to be unlearned, it is by default
+    added to the "least success" box.
+    '''
+    boxes['low'].append(card)
+    return boxes
 
 def test():
     card1 = Card(prompt="2+2=___", answer="4")
