d7569b31b6bfc8aee081c9a0d995266ce82f345d
Peter Lawson <lawsonpd@pm.me>
2022-06-22 19:35:26 -0500

    Add 'add_cardset' template. Add cardsets routes. Add query to fetch cardsets at /cardsets endpoint and corresponding template logic.

--- cardapp/cardsets.py
+++ cardapp/cardsets.py
@@ -7,9 +7,25 @@ bp = Blueprint('cardsets', __name__, url_prefix='/cardsets')
 
 @bp.route('')
 def index():
-    return render_template('cardsets/cardsets.html')
+    db = get_db()
+    cardsets = db.execute(
+        'SELECT * FROM cardset'
+    ).fetchall()
+    return render_template('cardsets/cardsets.html', cardsets=cardsets)
 
 @bp.route('/new', methods=('GET', 'POST'))
 def add_cardset():
-    return "Add new cardset"
+    return render_template('cardsets/add_cardset.html')
+
+@bp.route('/<int:cardset_id>')
+def view_cardset(cardset_id):
+    pass
+
+@bp.route('/<int:cardset_id>/learn')
+def learn_cardset(cardset_id):
+    pass
+
+@bp.route('/<int:cardset_id>/edit', methods=('GET', 'POST'))
+def edit_cardset(cardset_id):
+    pass
 
